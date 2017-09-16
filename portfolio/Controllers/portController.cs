49a64fc12caab4ae4ca87fc96009d530378aa485
shiva <shiva.sam811@gmail.com>
2017-09-16 00:33:21 -0400

    first commit

---
+++ portfolio/Controllers/portController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace portfolio.Controllers
+{
+    public class portController : Controller
+    {
+        // GET: port
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Profile1()
+        {
+            return View();
+        }
+        public ActionResult Resume()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Work()
+        {
+            return View();
+        }
+    }
+}
